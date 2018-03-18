ddc29d956de77a291f0008d09a6387b4ed9ad0ac
Faltenreich <philipp.fahlteich@gmail.com>
2018-03-18 12:34:16 +0100

    Replaced fab for toggling skeleton with menu

--- app/src/main/kotlin/com/faltenreich/skeletonlayout/MainActivity.kt
+++ app/src/main/kotlin/com/faltenreich/skeletonlayout/MainActivity.kt
@@ -2,6 +2,8 @@ package com.faltenreich.skeletonlayout
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toast
 import com.faltenreich.skeletonlayout.fragment.BaseSkeletonFragment
 import com.faltenreich.skeletonlayout.fragment.BottomSheetFragment
@@ -20,22 +22,39 @@ class MainActivity : AppCompatActivity() {
         init()
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return when (item?.itemId) {
+            R.id.action_toggle -> {
+                toggleSkeleton()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
     private fun init() {
         viewPagerAdapter = MainPagerAdapter(supportFragmentManager, arrayOf(RecyclerViewFragment(), ViewGroupFragment()))
         viewPager.adapter = viewPagerAdapter
         tabLayout.setupWithViewPager(viewPager)
 
-        toggleButton.setOnClickListener {
-            val skeleton = (viewPagerAdapter.getItem(viewPager.currentItem) as? BaseSkeletonFragment)?.getSkeleton()
-            skeleton?.let { if (it.isSkeleton()) it.showOriginal() else it.showSkeleton() }
-        }
+        fab.setOnClickListener { openEditor() }
+    }
 
-        editButton.setOnClickListener {
-            val visibleFragment = viewPagerAdapter.getItem(viewPager.currentItem)
-            when (visibleFragment) {
-                is BaseSkeletonFragment -> BottomSheetFragment.newInstance(visibleFragment).show(supportFragmentManager, "bottomSheet")
-                else -> Toast.makeText(this, "Unsupported", Toast.LENGTH_LONG).show()
-            }
+    private fun toggleSkeleton() {
+        val skeleton = (viewPagerAdapter.getItem(viewPager.currentItem) as? BaseSkeletonFragment)?.getSkeleton()
+        skeleton?.let { if (it.isSkeleton()) it.showOriginal() else it.showSkeleton() }
+    }
+
+    private fun openEditor() {
+        val visibleFragment = viewPagerAdapter.getItem(viewPager.currentItem)
+        when (visibleFragment) {
+            is BaseSkeletonFragment -> BottomSheetFragment.newInstance(visibleFragment).show(supportFragmentManager, "bottomSheet")
+            else -> Toast.makeText(this, "Unsupported", Toast.LENGTH_LONG).show()
         }
     }
 }
