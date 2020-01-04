357d48959458d8221795dc1f94cb51f3d6439cf5
Faltenreich <philipp.fahlteich@gmail.com>
2020-01-04 11:11:43 +0100

    Fixed java.lang.IllegalStateException: The specified child already has a parent by replacing masked views that already have a parent with the newly created SkeletonLayout. Fixes #14

--- skeletonlayout/src/main/kotlin/com/faltenreich/skeletonlayout/SkeletonExtensions.kt
+++ skeletonlayout/src/main/kotlin/com/faltenreich/skeletonlayout/SkeletonExtensions.kt
@@ -3,6 +3,7 @@
 package com.faltenreich.skeletonlayout
 
 import android.view.View
+import android.view.ViewGroup
 import androidx.annotation.ColorInt
 import androidx.annotation.LayoutRes
 import androidx.core.content.ContextCompat
@@ -23,12 +24,26 @@ private const val LIST_ITEM_COUNT_DEFAULT = 3
  */
 @JvmOverloads
 fun View.createSkeleton(
-        @ColorInt maskColor: Int = ContextCompat.getColor(context, SkeletonLayout.DEFAULT_MASK_COLOR),
-        cornerRadius: Float = SkeletonLayout.DEFAULT_CORNER_RADIUS,
-        showShimmer: Boolean = SkeletonLayout.DEFAULT_SHIMMER_SHOW,
-        @ColorInt shimmerColor: Int = ContextCompat.getColor(context, SkeletonLayout.DEFAULT_SHIMMER_COLOR),
-        shimmerDurationInMillis: Long = SkeletonLayout.DEFAULT_SHIMMER_DURATION_IN_MILLIS
-): Skeleton = SkeletonLayout(this, maskColor, cornerRadius, showShimmer, shimmerColor, shimmerDurationInMillis)
+    @ColorInt maskColor: Int = ContextCompat.getColor(context, SkeletonLayout.DEFAULT_MASK_COLOR),
+    cornerRadius: Float = SkeletonLayout.DEFAULT_CORNER_RADIUS,
+    showShimmer: Boolean = SkeletonLayout.DEFAULT_SHIMMER_SHOW,
+    @ColorInt shimmerColor: Int = ContextCompat.getColor(context, SkeletonLayout.DEFAULT_SHIMMER_COLOR),
+    shimmerDurationInMillis: Long = SkeletonLayout.DEFAULT_SHIMMER_DURATION_IN_MILLIS
+): Skeleton {
+    // If this View already has a parent, we need to replace it there with the SkeletonLayout
+    val parent = (parent as? ViewGroup)
+    val index = parent?.indexOfChild(this) ?: 0
+    val params = layoutParams
+
+    parent?.removeView(this)
+
+    val skeleton = SkeletonLayout(this, maskColor, cornerRadius, showShimmer, shimmerColor, shimmerDurationInMillis)
+
+    skeleton.layoutParams = params
+    parent?.addView(skeleton, index)
+
+    return skeleton
+}
 
 /**
  * Applies a new Skeleton to the given RecyclerView and wraps its ViewHolders' itemViews in SkeletonLayouts
@@ -43,11 +58,11 @@ fun View.createSkeleton(
  */
 @JvmOverloads
 fun RecyclerView.applySkeleton(
-        @LayoutRes listItemLayoutResId: Int,
-        itemCount: Int = LIST_ITEM_COUNT_DEFAULT,
-        @ColorInt maskColor: Int = ContextCompat.getColor(context, SkeletonLayout.DEFAULT_MASK_COLOR),
-        cornerRadius: Float = SkeletonLayout.DEFAULT_CORNER_RADIUS,
-        showShimmer: Boolean = SkeletonLayout.DEFAULT_SHIMMER_SHOW,
-        @ColorInt shimmerColor: Int = ContextCompat.getColor(context, SkeletonLayout.DEFAULT_SHIMMER_COLOR),
-        shimmerDurationInMillis: Long = SkeletonLayout.DEFAULT_SHIMMER_DURATION_IN_MILLIS
+    @LayoutRes listItemLayoutResId: Int,
+    itemCount: Int = LIST_ITEM_COUNT_DEFAULT,
+    @ColorInt maskColor: Int = ContextCompat.getColor(context, SkeletonLayout.DEFAULT_MASK_COLOR),
+    cornerRadius: Float = SkeletonLayout.DEFAULT_CORNER_RADIUS,
+    showShimmer: Boolean = SkeletonLayout.DEFAULT_SHIMMER_SHOW,
+    @ColorInt shimmerColor: Int = ContextCompat.getColor(context, SkeletonLayout.DEFAULT_SHIMMER_COLOR),
+    shimmerDurationInMillis: Long = SkeletonLayout.DEFAULT_SHIMMER_DURATION_IN_MILLIS
 ): Skeleton = SkeletonRecyclerView(this, listItemLayoutResId, itemCount, maskColor, cornerRadius, showShimmer, shimmerColor, shimmerDurationInMillis)
